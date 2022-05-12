19cce9d3a567e1f378b51aec6f09b7968576b840
Marco Tullio <tulliomarco93@gmail.com>
2022-05-11 23:26:28 -0300

    Menu

--- Problema_da_mochila/src/com/company/Main.java
+++ Problema_da_mochila/src/com/company/Main.java
@@ -9,16 +9,33 @@ public class Main {
         int  capacidaeMochila = 5, quantidadeItensAleatorios = 10;
          //int  capacidaeMochila = 15, quantidadeItensAleatorios = 3000000;
 
+        System.out.println("Menu");
+        System.out.println("1-Quantidade de itens resolvidos em 4 segundos\n2-teste automatizado");
+
         Item aleatorio = new Item();
         Item[] items =  aleatorio.geradorDeItens(capacidaeMochila,quantidadeItensAleatorios);
         Mochila mochila = new Mochila(items, capacidaeMochila);
+
+        switch (entrada.nextInt())
+        {
+            case  1:
+
+                break;
+            case  2:
+
+                break;
+
+            default:
+                break;
+
+        }
         mochila.exibir();
 
         long tempoInicial = System.currentTimeMillis();
 
 
         Solucao solutionBruteForce = mochila.resolverBruteForce();
-        Solucao Greedy = mochila.resolverGreedy("peso");
+        Solucao Greedy = mochila.resolverGreedy("valor");
         long tempoFinal = System.currentTimeMillis() -tempoInicial;
         //Syst em.out.println((tempoFinal /60));
         System.out.println("\nForca bruta");
