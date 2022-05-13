45d1bf0f4063b3f1a4dc5448f5eae362e80fb01c
Marco Tullio <tulliomarco93@gmail.com>
2022-05-13 20:50:39 -0300

    inicio da parte 1

--- Problema_da_mochila/src/com/company/Main.java
+++ Problema_da_mochila/src/com/company/Main.java
@@ -1,37 +1,52 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int  capacidadeMochila = 5, quantidadeItensAleatorios = 10;
+        int capacidadeMochila = 5, quantidadeItensAleatorios = 1;
 
+        //region Inicializacao de variaveis
         Item aleatorio = new Item();
-        Item[] items =  aleatorio.geradorDeItens(capacidadeMochila, quantidadeItensAleatorios);
+        Item[] items = aleatorio.geradorDeItens(capacidadeMochila, quantidadeItensAleatorios);
         Mochila mochila = new Mochila(items, capacidadeMochila);
 
         Solucao solutionBruteForce;
         Solucao solutionGreedy;
+        //endregion
 
         int qtdExecucoesBrute = 0;
         int itensBrute = 0;
         int itensGreedy = 20;
         int qtdExecucoesGreedy = 0;
 
-        long tempoInicial = System.currentTimeMillis();
-        long tempoFinal = 0;   
+        long tempoInicial = 0;
+        long tempoFinal = 0;
 
-        // System.out.println("1 - Quantidade de itens resolvidos em 4 segundos:");
-      
-        // while (tempoFinal < 4000){
-        //     solutionBruteForce = mochila.resolverBruteForce();
-        //     tempoFinal = System.currentTimeMillis() - tempoInicial;
-        //     qtdExecucoesBrute++;
-        // }
+        //region Parte 1
+        System.out.println("\n1 - Quantidade de itens resolvidos em 4 segundos:\n");
+
+        for (quantidadeItensAleatorios = 25; tempoFinal < 4000; quantidadeItensAleatorios++) {
+            items = aleatorio.geradorDeItens(capacidadeMochila, quantidadeItensAleatorios);
+            mochila = new Mochila(items, capacidadeMochila);
+            tempoInicial = System.currentTimeMillis();
+            solutionBruteForce = mochila.resolverBruteForce();
+            tempoFinal = System.currentTimeMillis() - tempoInicial;
+        }
+
+        System.out.println("Foi gerado uma quantidade de " + quantidadeItensAleatorios + " itens aleatorios dentro de 4 segundos");
+
+        //System.currentTimeMillis()
+
+
+        //endregion
+
+
         // itensBrute = qtdExecucoesBrute * quantidadeItensAleatorios;
         // System.out.println();
         // System.out.println("A mochila executou " + qtdExecucoesBrute +" vezes utilizando o 'BruteForce' e consegiu resolver " + itensBrute + " itens dentro de 4 segundos");
-                
+
         // tempoInicial = System.currentTimeMillis();
         // tempoFinal = 0;
 
@@ -47,7 +62,7 @@ public class Main {
         mochila.exibir();
 
         Item[] setItens;
-        Mochila mochilaBrute,mochilaGreedy;
+        Mochila mochilaBrute, mochilaGreedy;
 
         int setsIguais = 0;
 
@@ -58,8 +73,8 @@ public class Main {
             //Mochila mochilaGreedy = new Mochila(itemsGreedy, capacidadeMochila);
             solutionBruteForce = mochilaBrute.resolverBruteForce();
             solutionGreedy = mochilaGreedy.resolverGreedy("valorPorPeso");
-          
-            if(solutionBruteForce.value == solutionGreedy.value &&  solutionGreedy.value != 0){
+
+            if (solutionBruteForce.value == solutionGreedy.value && solutionGreedy.value != 0) {
                 solutionBruteForce.exibir();
                 setsIguais++;
                 System.out.println("\n");
@@ -69,7 +84,6 @@ public class Main {
         System.out.println("Fim da execucao");
         System.out.println(setsIguais);
     }
-        
-    long tempoInicial = System.currentTimeMillis();
-    long tempoFinal = System.currentTimeMillis() - tempoInicial;
+
+
 }
